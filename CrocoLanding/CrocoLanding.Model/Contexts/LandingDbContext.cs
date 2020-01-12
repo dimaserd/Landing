6892b7fa9937491db398332334e13a863f99a5a2
Дмитрий Сердюков <dimaserd84@gmail.com>
2020-01-12 15:08:45 +0300

    Init

---
+++ CrocoLanding/CrocoLanding.Model/Contexts/LandingDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CrocoShop.Model.Contexts
+{
+    public class LandingDbContext : DbContext
+    {
+        public const string ServerConnection = "ServerConnection";
+
+        public const string LocalConnection = "DefaultConnection";
+
+#if DEBUG
+        public static string ConnectionString => LocalConnection;
+#else
+        public static string ConnectionString => ServerConnection;
+#endif
+
+        #region Конструкторы
+        public LandingDbContext(DbContextOptions<LandingDbContext> options) : base(options)
+        {
+        }
+        #endregion
+
+        public static LandingDbContext Create(IConfiguration configuration)
+        {
+            return Create(configuration.GetConnectionString(ConnectionString));
+        }
+
+        public static LandingDbContext Create(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LandingDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new LandingDbContext(optionsBuilder.Options);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ CrocoLanding/CrocoLanding/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrocoLanding.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

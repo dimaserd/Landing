f5b6af3b3d300caa34521367c617b93e63582510
Дмитрий Сердюков <dimaserd84@gmail.com>
2020-03-19 21:33:20 +0300

    Minor

---
+++ CrocoLanding/Zoo/SimpleCrud/SimpleCrud.cs
@@ -0,0 +1,30 @@
+﻿using Croco.Core.Abstractions;
+using Croco.Core.Abstractions.Data.Entities.HaveId;
+using Croco.Core.Logic.Workers;
+using Croco.Core.Search.Models;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Zoo.SimpleCrud
+{
+    public class SimpleCrudServiceForStringId<TEntity, TModel, TCreateOrUpdateModel> : BaseCrocoWorker 
+        where TEntity : class, IHaveStringId
+        where TModel : class
+    {
+        public SimpleCrudServiceForStringId(Expression<Func<TEntity, TModel>> selectExpression, ICrocoAmbientContext context) : base(context)
+        {
+            SelectExpression = selectExpression;
+        }
+
+        public Expression<Func<TEntity, TModel>> SelectExpression { get; }
+
+        public Task<GetListResult<TModel>> GetList(GetListSearchModel model)
+        {
+            return GetListResult<TModel>.GetAsync(model, Query<TEntity>().OrderBy(x => x.Id), SelectExpression);
+        }
+
+
+    }
+}

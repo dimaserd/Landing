c5d0516f4453cb8c5cf407785003335512935d51
dms <dms@atechlab.eu>
2020-03-24 16:00:38 +0300

    Migration

---
+++ src/CrocoLanding/CrocoLanding.Model/Migrations/20200324125830_EccLinkCatchCreatedOn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrocoLanding.Model.Migrations
+{
+    public partial class EccLinkCatchCreatedOn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOnUtc",
+                table: "EmailLinkCatches",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOnUtc",
+                table: "EmailLinkCatches");
+        }
+    }
+}

ecd44bd8369228dbb5135841e59c16d9c6e6c928
dimaserd84@gmail.com <dimaserd84@gmail.com>
2021-07-28 18:13:28 +0300

    Minor

---
+++ src/CrocoLanding/CrocoLanding.Api/Controllers/EccController.cs
@@ -0,0 +1,41 @@
+﻿using Croco.Core.Abstractions.Models;
+using CrocoLanding.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CrocoLanding.Api.Controllers
+{
+    [Route("api/[controller]"), ApiController]
+    public class EccController : Controller
+    {
+        [HttpPost("SendCallBackRequest")]
+        public async Task<BaseApiResponse> SendCallBackRequest([FromForm] CreateCallBackApiModel model)
+        {
+            try
+            {
+                var ip = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+                return await SendCallBackRequestInnner(model, ip);
+            }
+            catch (Exception ex)
+            {
+                return new BaseApiResponse(ex);
+            }
+        }
+
+
+        private Task<BaseApiResponse> SendCallBackRequestInnner(CreateCallBackApiModel model, string ip)
+        {
+            var nModel = new CreateCallBackRequest
+            {
+                EmailOrPhoneNumber = model.EmailOrPhoneNumber,
+                Ip = ip
+            };
+
+            return CrocoTransactionHandler.System.ExecuteAndCloseTransaction(amb =>
+            {
+                return new CallBackRequestWorker(amb, IsDevelopment).CreateCallBackRequest(nModel);
+            });
+        }
+    }
+}
